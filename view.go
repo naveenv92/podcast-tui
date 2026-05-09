a61986353cfdcbe5f73a30269a401b4ebcf9a427
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2026-05-08 23:47:54 -0700

    Add two line breaks between home menu and listening stats

--- view.go
+++ view.go
@@ -122,7 +122,7 @@ func (m model) View() string {
 			}
 		}
 		content += "\n" + faintStyle.Render("↑/↓ navigate · enter select · q quit")
-		content += m.renderStatsBlock()
+		content += "\n" + m.renderStatsBlock()
 	case viewInProgress:
 		content = titleStyle.Render("IN-PROGRESS EPISODES") + "\n\n"
 		for i, item := range m.inProgressItems {
